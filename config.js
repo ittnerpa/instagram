ad612ca466dd6d81d06dca979d90a724ed02b44e
Ittner <patrick.ittner@wechselgott.com>
2022-03-14 14:52:14 +0100

    init project

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGODB:
-    'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
-  SECRET_KEY: 'some very secret key'
-};
-// ittnerpa60944
-// 7chp9trHZyPJxAp
